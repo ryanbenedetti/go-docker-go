23f708cf7e79f3429c6e908bc6bfe6423422ccd7
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2022-03-11 23:01:54 -0800

    add postAlbums and getAlbumById

--- main.go
+++ main.go
@@ -21,23 +21,61 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-// Logic to prepare responses
+// Logic for REST responses
 
-// getAlbums responds with the list of ALL albums as JSON.
+// GET: getAlbums responds with the list of ALL albums as JSON.
 func getAlbums(c *gin.Context) {
 
 	//serialize the struct into JSON and add it to the response.
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-// Code to map the request path to the response logic
+// POST: postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// BindJSON binds the received JSON to a newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the albums slice.
+	albums = append(albums, newAlbum)
+
+	// Add a 201 status code to the response, along with the album JSON
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
 
 func main() {
 	// Initialize a Gin router using Default.
 	router := gin.Default()
 
-	// GET HTTP method associates the /albums path with the getAlbums handler
+	// router maps the request path to the response logic
+
+	// GET HTTP method associates /albums path with getAlbums handler
 	router.GET("/albums", getAlbums)
+
+	// GET HTTP method associates /albums/id path with getAlbums instance
+	router.GET("/albums/:id", getAlbumByID)
+
+	// POST HTTP method associates /albums path with postAlbums handler
+	router.POST("/albums", postAlbums)
 
 	// Run attaches the router to an http.Server and starts the server
 	router.Run("localhost:8080")
